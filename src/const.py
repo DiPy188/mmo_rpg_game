01edcf560e0e9b44dc385c370b82473a601105dc
DiPy188 <DiPy188@gmail.com>
2023-08-16 00:27:01 +0300

    3

---
+++ src/const.py
@@ -0,0 +1,9 @@
+from typing import Tuple
+
+# player's direction of view
+LOOK_RIGHT: int = 0
+LOOK_LEFT: int = 1
+LOOK_UP: int = 2
+LOOK_DOWN: int = 3
+
+ENTITY_SIZE: Tuple[int, int] = (50, 100)  # Удалить
