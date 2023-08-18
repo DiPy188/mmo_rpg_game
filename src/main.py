5c6e29bc202cda3720fb40a2027c3cdc465518d4
DiPy188 <DiPy188@gmail.com>
2023-08-18 21:29:16 +0300

    первые штрихи сохранения

--- src/main.py
+++ src/main.py
@@ -1,8 +1,36 @@
 from typing import Tuple
 
+import os.path
+import csv
+
 import pygame as pg
 
-from entitys import Player, Enemy, enemy_gr, bullet_gr, player_gr, get_player
+from entitys import Player, Enemy, enemy_gr, bullet_gr, player_gr, get_player, scores
+
+
+def safe_scores(_scores: int,
+                _dir: str = '../data/safes/',
+                title: str = 'dev_safe1',
+                mode: str = 'w'):  # В будущем исправить на более универсальную функцию
+    _dir = os.path.abspath(_dir)
+
+    if not os.path.exists(_dir):
+        raise ValueError(f"Путь сохранения не корректен: {_dir}")
+
+    file_name = title + '.csv'
+    full_path = _dir + file_name
+
+    if not os.path.isfile(full_path) and mode == 'a':
+        raise ValueError(f"Режим записи не корректен: {mode=}")
+
+    data = [['scores'],
+            ['scores']]  # Со временем дополнять
+
+    with open(file=full_path, mode=mode, encoding='utf-8') as f:
+        writer = csv.writer(f)
+
+        for row in data:
+            writer.writerow(row)
 
 
 class App:
